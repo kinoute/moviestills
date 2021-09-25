78acb7e04f3f0d4ae4e25dbba583e3919b0ae39c
Yann Defretin <yann@defret.in>
2021-09-25 23:06:58 +0200

    remove SQLite storage / improve CLI args

--- main.go
+++ main.go
@@ -7,11 +7,26 @@ import (
     "strings"
     "time"
 
-    arg "github.com/alexflint/go-arg"
+    "github.com/alexflint/go-arg"
     "github.com/gocolly/colly/v2"
     "github.com/gocolly/colly/v2/extensions"
-    "github.com/velebak/colly-sqlite3-storage/colly/sqlite3"
 )
+
+const VERSION string = "0.1.0"
+
+// handle arguments passed through the CLI
+type args struct {
+    Website string        `arg:"required,-w, --website" help:"Website to scrap movie stills on"`
+    Delay   time.Duration `arg:"-d, --delay" help:"Delay in seconds to avoid getting banned" default:"2s"`
+}
+
+func (args) Description() string {
+    return "this program can scrap various websites to get high quality movie snapshots."
+}
+
+func (args) Version() string {
+    return "moviestills " + VERSION
+}
 
 func main() {
 
@@ -22,12 +37,7 @@ func main() {
         "blusscreens": websites.BlusScraper,
     }
 
-    // ask for website to scrap through arguments
-    var args struct {
-        Website string        `arg:"required,-w, --website" help:"Website to scrap movie stills on"`
-        Delay   time.Duration `arg:"-d, --delay" help:"Delay in seconds to avoid getting banned" default:"5s"`
-    }
-
+    var args args
     params := arg.MustParse(&args)
 
     // check website argument CLI
@@ -35,10 +45,10 @@ func main() {
         params.Fail("A website must be set through arguments.")
     }
 
-    // verify if we have a scrapper for the given website
+    // verify if we have a scrapper for the given website.
     // if we do, site_func will now be a function listed in
-    // the sites map that matches a module for this specifi
-    // website.
+    // the sites map that matches a module for this specific
+    // website stored in the "websites" folder.
     site_func, site_available := sites[strings.ToLower(args.Website)]
     if !site_available {
         log.Println("We don't have a scraper for this website.")
@@ -49,25 +59,12 @@ func main() {
         os.Exit(1)
     }
 
-    // if we're here, it means we have a valid scraper
-    // for a valid website!
+    // if we're here, it means we have a valid scraper for a valid website!
 
-    // Instantiate collector
+    // Instantiate main scraper
     scraper := colly.NewCollector(
         colly.CacheDir("./cache"),
     )
-
-    // save state of the scrapping on disk
-    storage := &sqlite3.Storage{
-        Filename: "./progress.db",
-    }
-
-    defer storage.Close()
-
-    err := scraper.SetStorage(storage)
-    if err != nil {
-        panic(err)
-    }
 
     // use random user agent and referer to
     // avoid getting banned
@@ -81,10 +78,8 @@ func main() {
         RandomDelay: args.Delay * time.Second,
     })
 
-    // here we call the website module:
-    // depending on the website provided
-    // in the CLI, it will call a different
-    // file/module/function made specifically
+    // here we call the website module depending on the website provided
+    // in the CLI. We will call a different file/module/function made specifically
     // to scrap this website.
     site_func(&scraper)
 }
