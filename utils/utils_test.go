2b8da0ddd6b9e0784adcf1c7a7f19e654ba680fe
Yann Defretin <yann@defret.in>
2021-09-30 09:00:27 +0200

    add tests for utils

---
+++ utils/utils_test.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestNormalize(t *testing.T) {
+	cases := []struct {
+		in   string
+		want string
+	}{
+		{"Les misérables", "Les miserables"},
+		{" Kagerô-za", "Kagero-za"},
+		{" Méliès: Fairy Tales in Color", "Melies: Fairy Tales in Color"},
+		{" Love (2D  +    3D) ", "Love (2D + 3D)"},
+		{"", ""},
+	}
+
+	for _, c := range cases {
+		got, _ := Normalize(c.in)
+		if got != c.want {
+			t.Errorf("Normalize(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
+func TestRemoveURLParams(t *testing.T) {
+	cases := []struct {
+		in   string
+		want string
+	}{
+		{"https://defret.in/log/dark-theme-on-ruby-on-rails", "https://defret.in/log/dark-theme-on-ruby-on-rails"},
+		{"https://www.youtube.com/watch?v=LvgVSSpwND8", "https://www.youtube.com/watch"},
+		{"https://i1.wp.com/caps.pictures/198/0-airplane/full/airplane-movie-screencaps.com-1.jpg?strip=all",
+			"https://i1.wp.com/caps.pictures/198/0-airplane/full/airplane-movie-screencaps.com-1.jpg"},
+		{"https://film-grab.com/wp-content/uploads/photo-gallery/imported_from_media_libray/10cloverfield001.jpg?bwg=1546957525",
+			"https://film-grab.com/wp-content/uploads/photo-gallery/imported_from_media_libray/10cloverfield001.jpg"},
+		{"https://stillsfrmfilms.files.wordpress.com/2014/02/012.jpg?w=150&amp;h=64",
+			"https://stillsfrmfilms.files.wordpress.com/2014/02/012.jpg"},
+		{"", ""},
+	}
+
+	for _, c := range cases {
+		got := RemoveURLParams(c.in)
+		if got != c.want {
+			t.Errorf("RemoveURLParams(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
