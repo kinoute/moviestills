0177099d8b5539e9ecea986960c89cba2ae9d55d
Yann Defretin <yann@defret.in>
2021-10-03 19:53:05 +0200

    get rid of time delay

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"moviestills/websites"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/alexflint/go-arg"
 	"github.com/gocolly/colly/v2"
@@ -17,12 +16,11 @@ const VERSION string = "0.1.0"
 
 // Handle arguments passed through the CLI
 type args struct {
-	Website  string        `arg:"required,-w, --website,env:WEBSITE" help:"Website to scrap movie stills on"`
-	Delay    time.Duration `arg:"-t, --time,env:DELAY" help:"Delay in seconds to avoid getting banned" default:"2s"`
-	Parallel int           `arg:"-p, --parallel,env:PARALLEL" help:"Limit the maximum parallelism" default:"2"`
-	Async    bool          `arg:"-a, --async,env:ASYNC" help:"Enable asynchronus running job"`
-	CacheDir string        `arg:"-c, --cache-dir,env:CACHE_DIR" help:"Where to cache scraped websites pages" default:"cache"`
-	Debug    bool          `arg:"-d, --debug,env:DEBUG" help:"Enable debugging for Colly, our scraper"`
+	Website  string `arg:"required,-w, --website,env:WEBSITE" help:"Website to scrap movie stills on"`
+	Parallel int    `arg:"-p, --parallel,env:PARALLEL" help:"Limit the maximum parallelism" default:"2"`
+	Async    bool   `arg:"-a, --async,env:ASYNC" help:"Enable asynchronus running job"`
+	CacheDir string `arg:"-c, --cache-dir,env:CACHE_DIR" help:"Where to cache scraped websites pages" default:"cache"`
+	Debug    bool   `arg:"-d, --debug,env:DEBUG" help:"Enable debugging for Colly, our scraper"`
 }
 
 func (args) Description() string {
@@ -105,7 +103,6 @@ func main() {
 	if err := scraper.Limit(&colly.LimitRule{
 		DomainGlob:  "*",
 		Parallelism: args.Parallel,
-		RandomDelay: args.Delay * time.Second,
 	}); err != nil {
 		log.Println("Can't change scraper limit options:", err)
 	}
