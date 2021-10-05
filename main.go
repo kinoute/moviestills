da2bf89fad8dc88aaa42b6954f4b9e2afd98c12f
Yann Defretin <yann@defret.in>
2021-10-05 12:55:48 +0200

    improve description of arguments

--- main.go
+++ main.go
@@ -19,9 +19,9 @@ const VERSION string = "0.1.0"
 type args struct {
 	Website  string `arg:"required,-w, --website,env:WEBSITE" help:"Website to scrap movie stills on"`
 	Parallel int    `arg:"-p, --parallel,env:PARALLEL" help:"Limit the maximum parallelism" default:"2"`
-	Async    bool   `arg:"-a, --async,env:ASYNC" help:"Enable asynchronus running job"`
+	Async    bool   `arg:"-a, --async,env:ASYNC" help:"Enable asynchronus running jobs"`
 	CacheDir string `arg:"-c, --cache-dir,env:CACHE_DIR" help:"Where to cache scraped websites pages" default:"cache"`
-	Debug    bool   `arg:"-d, --debug,env:DEBUG" help:"Enable debugging for Colly, our scraper"`
+	Debug    bool   `arg:"-d, --debug,env:DEBUG" help:"Enable Colly Debugger, our scraper"`
 }
 
 func (args) Description() string {
