cc805139abf1491af269ed78ded75b2dd8baec14
Yann Defretin <yann@defret.in>
2021-10-10 17:37:15 +0200

    WIP: better logging

--- main.go
+++ main.go
@@ -1,31 +1,35 @@
 package main
 
 import (
-	"log"
 	"moviestills/config"
 	"moviestills/utils"
 	"moviestills/websites"
+	"os"
+	"reflect"
 	"strings"
 
 	"github.com/alexflint/go-arg"
 	"github.com/gocolly/colly/v2"
 	"github.com/gocolly/colly/v2/debug"
 	"github.com/gocolly/colly/v2/extensions"
+	log "github.com/pterm/pterm"
 )
 
 func main() {
 
+	clearScreen()
+
 	// Implemented scrapers as today
 	sites := map[string]func(**colly.Collector, *config.Options){
-		"blubeaver":        websites.BluBeaverScraper,
-		"blusscreens":      websites.BlusScraper,
-		"dvdbeaver":        websites.DVDBeaverScraper,
-		"evanerichards":    websites.EvanERichardsScraper,
-		"film-grab":        websites.FilmGrabScraper,
-		"highdefdiscnews":  websites.HighDefDiscNewsScraper,
-		"movie-screencaps": websites.ScreenCapsScraper,
-		"screenmusings":    websites.ScreenMusingsScraper,
-		"stillsfrmfilms":   websites.StillsFrmFilmsScraper,
+		"blubeaver": websites.BluBeaverScraper,
+		// "blusscreens":      websites.BlusScraper,
+		// "dvdbeaver":        websites.DVDBeaverScraper,
+		// "evanerichards":    websites.EvanERichardsScraper,
+		// "film-grab":        websites.FilmGrabScraper,
+		// "highdefdiscnews":  websites.HighDefDiscNewsScraper,
+		// "movie-screencaps": websites.ScreenCapsScraper,
+		// "screenmusings":    websites.ScreenMusingsScraper,
+		// "stillsfrmfilms":   websites.StillsFrmFilmsScraper,
 	}
 
 	// Handle arguments passed through the CLI or environment variables
@@ -33,9 +37,15 @@ func main() {
 	var options config.Options
 	arg.MustParse(&options)
 
+	log.DefaultHeader.Println("Movie Stills", config.VERSION)
+
+	log.DefaultSection.Println("Configuration")
+	printConfiguration(&options)
+
 	// Check presence of website argument
 	if options.Website == "" {
-		log.Fatalln("A website must be set through arguments.")
+		log.Error.Println("A website must be set through arguments.")
+		os.Exit(1)
 	}
 
 	// Verify if we have a scrapper for the given website.
@@ -44,12 +54,14 @@ func main() {
 	// website stored in the "websites" folder.
 	site_func, scraper_exists := sites[strings.ToLower(options.Website)]
 	if !scraper_exists {
-		log.Println("We don't have a scraper for this website.")
-		log.Println("List of available scrapers:")
+		log.Error.Println("We don't have a scraper for this website.")
+		log.Info.Println("List of available scrapers:")
 		for site := range sites {
-			log.Println("–", site)
+			log.Info.Println("–", site)
 		}
-		log.Fatalln("See how you can add support for a new website: https://github.com/kinoute/moviestills#contribute")
+		log.Info.Println("See how you can add support for a new website:",
+			log.White("https://github.com/kinoute/moviestills#contribute"))
+		os.Exit(1)
 	}
 
 	// If we're here, it means we have a valid scraper for a valid website!
@@ -58,14 +70,16 @@ func main() {
 	// This folder stores the scraped websites pages.
 	// If we can't create it, stop right there.
 	if _, err := utils.CreateFolder(options.CacheDir); err != nil {
-		log.Fatalln("The cache directory", options.CacheDir, "can't be created:", err)
+		log.Error.Println("The cache directory", log.White(options.CacheDir), "can't be created:", log.Red(err))
+		os.Exit(1)
 	}
 
 	// Create the "data" directory.
 	// This folder stores the movie snapshots.
 	// If we can't create it, stop right there.
 	if _, err := utils.CreateFolder(options.DataDir); err != nil {
-		log.Fatalln("The data directory", options.DataDir, "can't be created:", err)
+		log.Error.Println("The data directory", log.White(options.DataDir), "can't be created:", log.Red(err))
+		os.Exit(1)
 	}
 
 	// Instantiate main scraper
@@ -92,8 +106,10 @@ func main() {
 		DomainGlob:  "*",
 		Parallelism: options.Parallel,
 	}); err != nil {
-		log.Println("Can't change scraper limit options:", err)
+		log.Warning.Println("Can't change scraper limit options:", log.Red(err))
 	}
+
+	log.DefaultSection.Println("Scraping")
 
 	// Here we call the website module depending on the website provided
 	// in the CLI by the user.
@@ -101,4 +117,37 @@ func main() {
 	// All available scrapers are stored in the "websites" folder.
 	site_func(&scraper, &options)
 
+}
+
+// Clear Terminal Screen before startnig the app
+func clearScreen() {
+	print("\033[H\033[2J")
+}
+
+// Print configuration as a bullet list
+func printConfiguration(options *config.Options) {
+
+	// Get fields and its values from the config struct
+	values := reflect.ValueOf((*options))
+	fields := values.Type()
+
+	// Create bullet lists with configuration
+	configuration := []log.BulletListItem{}
+
+	for i := 0; i < values.NumField(); i++ {
+		configuration = append(configuration,
+			log.BulletListItem{
+				Level:       0,
+				Text:        log.Yellow(fields.Field(i).Name) + ": " + log.Blue((values.Field(i).Interface())),
+				TextStyle:   log.NewStyle(log.FgBlue),
+				BulletStyle: log.NewStyle(log.FgRed),
+			},
+		)
+	}
+
+	// Print the configuration as a bullet list
+	err := log.DefaultBulletList.WithItems(configuration).Render()
+	if err != nil {
+		log.Error.Println("Could not print configuration", log.Red(err))
+	}
 }
--- websites/blubeaver.go
+++ websites/blubeaver.go
@@ -1,13 +1,13 @@
 package websites
 
 import (
-	"log"
 	"moviestills/config"
 	"moviestills/utils"
 	"os"
 	"strings"
 
 	"github.com/gocolly/colly/v2"
+	log "github.com/pterm/pterm"
 )
 
 // This webpage stores a list of links to movie reviews of Blu-rays
@@ -15,7 +15,7 @@ const BluBeaverURL string = "http://www.dvdbeaver.com/blu-ray.htm"
 
 func BluBeaverScraper(scraper **colly.Collector, options *config.Options) {
 
-	log.Println("Starting BluBeaver Scraper...")
+	log.Info.Println("Started BluBeaver Scraper...")
 
 	// Change allowed domain for the main scraper.
 	// Since everything is served on the same domain,
@@ -46,12 +46,12 @@ func BluBeaverScraper(scraper **colly.Collector, options *config.Options) {
 
 	// Print error just in case
 	(*scraper).OnError(func(r *colly.Response, err error) {
-		log.Println(r.Request.URL, "\t", r.StatusCode, "\nError:", err)
+		log.Error.Println(r.Request.URL, "\t", r.StatusCode, "\nError:", log.Red(err))
 	})
 
 	// Before making a request print "Visiting ..."
 	(*scraper).OnRequest(func(r *colly.Request) {
-		log.Println("visiting index page", r.URL.String())
+		log.Debug.Println("visiting index page", log.White(r.URL.String()))
 	})
 
 	// Find links to movies reviews and isolate the movie's title.
@@ -64,26 +64,28 @@ func BluBeaverScraper(scraper **colly.Collector, options *config.Options) {
 		// on Amazon icons. Since we use the link to isolate the movie's title,
 		// we ignore these links as they don't have the movie's name included.
 		if _, iconExistsInLink := e.DOM.Find("img").Attr("src"); iconExistsInLink {
-			log.Println("Link without text, just an icon, next")
+			log.Debug.Println("Link without text, just an icon, next")
 			return
 		}
 
 		movieURL := e.Request.AbsoluteURL(e.Attr("href"))
-		log.Println("Found movie page link", movieURL)
+		log.Debug.Println("Found movie page link", log.White(movieURL))
 
 		// Remove weird accents and spaces from the movie's title
 		movieName, err := utils.Normalize(e.Text)
 		if err != nil || movieName == "" {
-			log.Println("Can't normalize Movie name for:", e.Text)
+			log.Error.Println("Can't normalize Movie name for", log.White(e.Text), ":", log.Red(err))
 			return
 		}
 
 		// Create folder to save images in case it doesn't exist
 		moviePath, err := utils.CreateFolder(options.DataDir, options.Website, movieName)
 		if err != nil {
-			log.Println("Error creating movie folder for:", movieName, err)
+			log.Error.Println("Can't create movie folder for:", log.White(movieName), log.Red(err))
 			return
 		}
+
+		log.Success.Println("Found movie page for:", log.Blue(movieName))
 
 		// Pass the movie's name and path to the next request context
 		// in order to save the images in correct folder.
@@ -92,12 +94,44 @@ func BluBeaverScraper(scraper **colly.Collector, options *config.Options) {
 		ctx.Put("movie_path", moviePath)
 
 		if err = movieScraper.Request("GET", movieURL, nil, ctx, nil); err != nil {
-			log.Println("Can't request movie page:", err)
+			log.Error.Println("Can't request movie page", log.White(movieURL), ":", log.Red(err))
 		}
 
 		// In case we enabled asynchronous jobs
 		movieScraper.Wait()
 	})
+
+	// Before making a request to movie review page
+	movieScraper.OnRequest(func(r *colly.Request) {
+		log.Debug.Println("visiting movie page", log.White(r.URL.String()))
+	})
+
+	// It's but sometimes on BD reviews there are no large versions.
+	// Therefore we download the images as shown on the webpage and
+	// be sure we avoid some weird ones (subtitles, DVD covers etc).
+	// movieScraper.OnHTML(
+	// 	"img:not([src*='banner' i])"+
+	// 		":not([src*='bitrate' i])"+
+	// 		":not([src$='gif' i])"+
+	// 		":not([src*='subs' i])"+
+	// 		":not([src*='daggers' i])"+
+	// 		":not([src*='posters' i])"+
+	// 		":not([src*='title' i])"+
+	// 		":not([src*='menu' i])", func(e *colly.HTMLElement) {
+	// 		movieImageURL := e.Request.AbsoluteURL(e.Attr("src"))
+
+	// 		// Filter low resolutions images to avoid false positives.
+	// 		// if the images are too small, we won't be able to use them
+	// 		// anyway so let's skip them.
+	// 		movieImageWidth, _ := strconv.Atoi(e.Attr("width"))
+	// 		movieImageHeight, _ := strconv.Atoi(e.Attr("height"))
+
+	// 		if movieImageHeight >= 275 && movieImageWidth >= 500 {
+	// 			if err := e.Request.Visit(movieImageURL); err != nil {
+	// 				log.Error.Println("Can't request linked image", log.White(movieImageURL), ":", log.Red(err))
+	// 			}
+	// 		}
+	// 	})
 
 	// Look for links on images that redirects to a "largest" version.
 	// These links appear on Blu-Ray reviews almost exclusively and
@@ -107,10 +141,10 @@ func BluBeaverScraper(scraper **colly.Collector, options *config.Options) {
 	// most likely images with subtitles on top. We don't want that.
 	movieScraper.OnHTML("a[href*='large' i]:not([href*='subs' i])", func(e *colly.HTMLElement) {
 		movieImageURL := e.Request.AbsoluteURL(e.Attr("href"))
-		log.Println("Found linked image", movieImageURL)
+		log.Debug.Println("Found linked image", log.White(movieImageURL))
 
 		if err := e.Request.Visit(movieImageURL); err != nil {
-			log.Println("Can't request linked image:", err)
+			log.Error.Println("Can't request linked image", log.White(movieImageURL), ":", log.Red(err))
 		}
 	})
 
@@ -125,17 +159,27 @@ func BluBeaverScraper(scraper **colly.Collector, options *config.Options) {
 			// Don't save again it we already downloaded it
 			if _, err := os.Stat(outputImgPath); os.IsNotExist(err) {
 				if err = r.Save(outputImgPath); err != nil {
-					log.Println("Can't save image:", err)
+					log.Error.Println("Can't save image", log.White(r.FileName()), ":", log.Red(err))
 				}
 			}
 			return
 		}
 	})
 
+	movieScraper.OnScraped(func(r *colly.Response) {
+		if strings.Contains(r.Headers.Get("Content-Type"), "image") {
+			log.Success.Println(
+				"Got image for",
+				log.Blue(r.Request.Ctx.Get("movie_name")),
+				log.White(utils.LimitLength(r.FileName(), 50)))
+		}
+	})
+
 	if err := (*scraper).Visit(BluBeaverURL); err != nil {
-		log.Println("Can't visit index page:", err)
+		log.Error.Println("Can't visit index page", log.White(BluBeaverURL), ":", log.Red(err))
 	}
 
 	// In case we enabled asynchronous jobs
 	(*scraper).Wait()
+
 }
